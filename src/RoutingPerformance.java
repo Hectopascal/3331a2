361e7095723ab5f7511889fa257baf689e106403
vampirate <vam-pirate@hotmail.com>
2017-09-28 00:11:53 +1000

    Added variables and arguments

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class RoutingPerformance {
 	public static void main(String[] args) {
-		new RoutingPerformance();
+		String networkScheme = args[0];
+		String routingScheme = args[1];
+		String topologyFile = args[2];
+		
+		try {
+			Scanner topologyScanner = new Scanner(new File(topologyFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		String workloadFile = args[3];
+		
+		try {
+			Scanner workloadScanner = new Scanner(new File(workloadFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		int packetRate = Integer.parseInt(args[4]);
+		
+		
 	}
 }
