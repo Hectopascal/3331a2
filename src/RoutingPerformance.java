f81b76f05da3f51e13ebfdd785834b147ccec3e9
Hectopascal <voracian@gmail.com>
2017-10-08 16:59:50 +1100

    adjusted stuff

--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -6,18 +6,31 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class RoutingPerformance {
+	String networkScheme;
+	String routingScheme;
+	int packetRate;
 	
 	public static void main(String[] args) {
+		RoutingPerformance rp = new RoutingPerformance();
+		rp.networkScheme = args[0];
+		rp.routingScheme = args[1];
 		
-		String networkScheme = args[0];
-		String routingScheme = args[1];
 		String topologyFile = args[2];
-		//String workloadFile = args[3];
+		String workloadFile = args[3];
+		
 		//int packetRate = Integer.parseInt(args[4]);
 		
 		
 		Graph g = new Graph(topologyFile);
 
+		/* 3 algorithms to implement (Dijkstras):
+		*
+		*1. least number of hops (take cost of edge to be = 1)
+		*2. least delay (cost of edge = propagation delay)
+		*
+		*3. least loaded (pick route where the 'narrowest' link that can accomodate highest capacity)
+		*/
+		
 		
 		
 	}
