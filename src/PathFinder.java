33b3f66779fc1e3a401505955799a0234b9c2c9f
Daniel Bi <vam-pirate@hotmail.com>
2017-10-17 15:30:38 +1100

    stuuf

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -7,20 +7,18 @@ import java.util.TreeSet;
 
 public class PathFinder {
 	
-	public HashMap<String,String> findPath(Graph g, String source, String dest, String routingScheme) {
+	public HashMap<String, String> findPath (Graph g, String source, String dest, String routingScheme) {
 		//TODO
-		if(routingScheme == "SHP") {
+		if (routingScheme == "SHP") {
 			
-		} else if(routingScheme == "LLP") {
+		} else if (routingScheme == "LLP") {
 			
 		}
 		
 		return findPathSDP(g, source, dest); //SDP
 	}
 	
-	public HashMap<String,String> findPathSDP(Graph g, String source, String dest) {
-		
-		
+	public HashMap<String, String> findPathSDP(Graph g, String source, String dest) {
 		HashMap<String,Integer> nodeCosts = new HashMap<String,Integer>();
 		HashMap<String, String> prevNode = new HashMap<String,String>();
 		HashMap<String, ArrayList<Link>> nodeList = new HashMap<String,ArrayList<Link>>(g.getNodes());
@@ -28,25 +26,22 @@ public class PathFinder {
 		nodeCosts.put(source, 0);
 		prevNode.put(source, null);
 		
-		while(!nodeList.isEmpty()) {
-			
-			
+		while (!nodeList.isEmpty()) {
 			 String cur = getMinimum(nodeList, nodeCosts);
 			 LinkedList<Link> neighbours = new LinkedList<Link>(nodeList.get(cur));
 			 
 			 nodeList.remove(cur);
 			 
-			 while(!neighbours.isEmpty()) {
+			 while (!neighbours.isEmpty()) {
 				 Link n = neighbours.removeFirst();
 				 String neighbourNode = n.otherEnd(cur);
 				 
-				 if(nodeCosts.containsKey(neighbourNode)) {
-					 if(nodeCosts.get(neighbourNode) > nodeCosts.get(cur)+n.getDelay()) {
+				 if (nodeCosts.containsKey(neighbourNode)) {
+					 if (nodeCosts.get(neighbourNode) > nodeCosts.get(cur)+n.getDelay()) {
 						 nodeCosts.put(neighbourNode, nodeCosts.get(cur)+n.getDelay());
 						 prevNode.put(neighbourNode, cur);
 					 }
-				 }else {
-					 
+				 } else {					 
 					 nodeCosts.put(neighbourNode, nodeCosts.get(cur)+n.getDelay());
 					 prevNode.put(neighbourNode, cur);
 				 }
@@ -59,8 +54,8 @@ public class PathFinder {
 	private String getMinimum(HashMap<String, ArrayList<Link>> nodeList, HashMap<String, Integer> nodeCosts) {
 		String minimum = null;
 		int minCost = Integer.MAX_VALUE;
-		for(String key : nodeList.keySet()) {
-			if(nodeCosts.containsKey(key) && nodeCosts.get(key)<minCost) {
+		for (String key : nodeList.keySet()) {
+			if (nodeCosts.containsKey(key) && nodeCosts.get(key)<minCost) {
 				minimum = key;
 				minCost = nodeCosts.get(key);
 			}
--- src/RoutingPerformance.java
+++ src/RoutingPerformance.java
@@ -23,12 +23,8 @@ public class RoutingPerformance {
 		
 		
 		String[] workloadFileArray = workloadFile.split("\n");
-		int packetRate = Integer.parseInt(args[4]);
 		rp.connections = rp.addConnections(workloadFile);
 		
-		
-		
-		//int packetRate = Integer.parseInt(args[4]);
 		
 		Graph g = new Graph(topologyFile);
 
@@ -44,17 +40,23 @@ public class RoutingPerformance {
 		PathFinder pf = new PathFinder();
 		ArrayList<String> route = new ArrayList<String>();
 		
-		String currentNode;
-		String destinationNode;
-		while (!rp.connections.isEmpty()) {
-			
+		String currentNode = "A";
+		String destinationNode = "A";
+		String tempNode = "A";
+		String tempNode2 = "A";
+		while (!rp.connections.isEmpty()) {//while there are still connections to be routed
 			//if connection is add {
-				//get the route from algo
+			//get the route from algo
 			if (rp.connections.firstEntry().getValue().split(" ")[0] == "1")
 				currentNode = rp.connections.firstEntry().getValue().split(" ")[1];
 				destinationNode = rp.connections.firstEntry().getValue().split(" ")[2];
 				s = pf.findPath(g, currentNode, destinationNode, rp.routingScheme);
-				
+				tempNode = destinationNode;
+				while (!s.isEmpty()) {
+					tempNode2 = s.get(tempNode);
+					s.remove(tempNode);
+					tempNode = tempNode2;
+				}
 				//update the map, add route onto another treemap
 				
 				//update statistics		
@@ -85,6 +87,7 @@ public class RoutingPerformance {
 		
 		while (connectionNumber < 10) {
 			currentPacket = sc.nextLine();
+			//System.out.println("currentpacket is " + currentPacket);
 			currentPacketTime = Float.parseFloat((currentPacket.split(" "))[0]);
 			currentPacketSource = (currentPacket.split(" "))[1];
 			currentPacketDestination = (currentPacket.split(" "))[2];
@@ -93,8 +96,8 @@ public class RoutingPerformance {
 			connections.put(currentPacketTime, "S " + currentPacketSource + " " + currentPacketDestination + " " + connectionNumber);
 			connections.put(currentPacketEndTime, "E " + currentPacketSource + " " + currentPacketDestination + " " + connectionNumber);
 			connectionNumber++;
-			System.out.println(connections.firstEntry());
-			connections.remove(connections.firstEntry().getKey());
+			//System.out.println(connections.firstEntry());
+			//connections.remove(connections.firstEntry().getKey());
 		}
 		return connections;
 		
